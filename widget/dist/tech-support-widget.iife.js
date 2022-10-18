274985ceb99c760d59bc109ba5f489ace34a8aee
Lupo Montero <lupomontero@gmail.com>
2022-10-18 11:44:21 +0200

    chore(dist): Regenera templates.json y actualiza dist

--- widget/dist/tech-support-widget.iife.js
+++ widget/dist/tech-support-widget.iife.js
@@ -17,7 +17,38 @@ el problema.
 
 **Contexto adicional**
 Agrega cualquier otra cosa que pueda ayudar a entender el contexto en el que
-ocurre el problema.`},{fname:"feature_request.es.md",id:"feature_request",lang:"es",headers:{name:"Sugiere una mejora",about:"Prop\xF3n una idea de mejora que te gustar\xEDa ver",title:"[Mejora] Reemplaza este texto con el t\xEDtulo",labels:"enhancement",assignees:""},body:`**La mejora que quieres proponer hace relaci\xF3n a un problema?**
+ocurre el problema.
+
+**Alerta**
+Recuerda que este repositorio es p\xFAblico, todos los issues y conversaciones 
+relacionadas son p\xFAblicas y visibles a todo el mundo.
+Si necesitas compartirnos informaci\xF3n de forma privada, NO lo hagas a trav\xE9s de este repo, 
+nos pondremos en contacto para poder dar soporte a trav\xE9s de un canal privado.`},{fname:"bug_report.pt.md",id:"bug_report",lang:"pt",headers:{name:"Reportar um problema",about:"Informe um problema que voc\xEA identificou para nos ajudar a melhorar",title:"[Bug] Substitua este texto pelo t\xEDtulo",labels:"bug",assignees:"",lang:"es"},body:`**Descreva o problema**
+Descri\xE7\xE3o clara e concisa de qual \xE9 o problema.
+
+**Para jogar**
+Etapas para reproduzir o problema:
+1. V\xE1 para '...'
+2. Clique em '....'
+3. Role para baixo at\xE9 '....'
+4. Veja o erro
+
+**Comportamento esperado**
+Descri\xE7\xE3o clara e concisa do que voc\xEA esperava que acontecesse.
+
+**Screenshots**
+Se poss\xEDvel, adicione capturas de tela para ajudar a entender
+o problema.
+
+**Contexto adicional**
+Adicione qualquer outra coisa que possa ajudar a entender o contexto em que
+o problema ocorre.
+
+**Alerta**
+Lembre-se que este reposit\xF3rio \xE9 p\xFAblico, todos os problemas e conversas
+relacionados s\xE3o p\xFAblicos e vis\xEDveis para todos.
+Se voc\xEA precisar compartilhar informa\xE7\xF5es conosco em particular, N\xC3O fa\xE7a isso por meio deste reposit\xF3rio,
+Entraremos em contato com voc\xEA para poder fornecer suporte por meio de um canal privado.`},{fname:"feature_request.es.md",id:"feature_request",lang:"es",headers:{name:"Sugiere una mejora",about:"Prop\xF3n una idea de mejora que te gustar\xEDa ver",title:"[Mejora] Reemplaza este texto con el t\xEDtulo",labels:"enhancement",assignees:""},body:`**La mejora que quieres proponer hace relaci\xF3n a un problema?**
 Descripci\xF3n clara y concisa de cu\xE1l es el problema. Por ejemplo, siempre me
 frustro cuando [...]
 
@@ -25,12 +56,48 @@ frustro cuando [...]
 Descripci\xF3n clara y concisa de qu\xE9 te gustar\xEDa que ocurra.
 
 **\xBFQu\xE9 alternativas has considerado?**
-Descripci\xF3n clara y concisa de otras solucioines alternativas que hayas
+Descripci\xF3n clara y concisa de otras soluciones alternativas que hayas
 considerado.
 
 **Contexto adicional**
 Agrega cualquier otra cosa que pueda ayudar a entender el contexto en el que
-ocurre el problema.`},{fname:"question.es.md",id:"question",lang:"es",headers:{name:"Haz una pregunta",about:"\xBFTienes alguna duda o comentario?",title:"[Pregunta] Reemplaza este texto con el t\xEDtulo",labels:"question",assignees:""},body:"En este espacio puedes agregar el contexto necesario: texto, screenshots, etc."}],g={es:{techSupport:"Tech support",close:"Cerrar"},pt:{techSupport:"Tech support",close:"Fechar"}},b=t=>r=>g[t][r]||r,h=t=>`
+ocurre el problema.
+
+**Alerta**
+Recuerda que este repositorio es p\xFAblico, todos los issues y conversaciones 
+relacionadas son p\xFAblicas y visibles a todo el mundo.
+Si necesitas compartirnos informaci\xF3n de forma privada, NO lo hagas a trav\xE9s de este repo, 
+nos pondremos en contacto para poder dar soporte a trav\xE9s de un canal privado.`},{fname:"feature_request.pt.md",id:"feature_request",lang:"pt",headers:{name:"Sugerir uma melhoria",about:"Proponha uma ideia de melhoria que voc\xEA gostaria de ver",title:"[Melhoria] Substitua este texto pelo t\xEDtulo",labels:"enhancement",assignees:""},body:`**A melhoria que voc\xEA deseja propor est\xE1 relacionada a um problema?**
+Descri\xE7\xE3o clara e concisa de qual \xE9 o problema. Por exemplo, Eu fico frustrado quando [...]
+
+**Descreva a solu\xE7\xE3o desejada**
+Descri\xE7\xE3o clara e concisa do que voc\xEA gostaria de ver acontecer.
+
+**Quais alternativas voc\xEA considerou?**
+Descri\xE7\xE3o clara e concisa de outras solu\xE7\xF5es alternativas que voc\xEA tem
+considerado.
+
+**Contexto adicional**
+Adicione qualquer outra coisa que possa ajudar a entender o contexto em que
+o problema ocorre.
+
+**Alerta**
+Lembre-se que este reposit\xF3rio \xE9 p\xFAblico, todos os problemas e conversas
+relacionados s\xE3o p\xFAblicos e vis\xEDveis para todos.
+Se voc\xEA precisar compartilhar informa\xE7\xF5es conosco em particular, N\xC3O fa\xE7a isso por meio deste reposit\xF3rio,
+Entraremos em contato com voc\xEA para poder fornecer suporte por meio de um canal privado.`},{fname:"question.es.md",id:"question",lang:"es",headers:{name:"Haz una pregunta",about:"\xBFTienes alguna duda o comentario?",title:"[Pregunta] Reemplaza este texto con el t\xEDtulo",labels:"question",assignees:""},body:`En este espacio puedes agregar el contexto necesario: texto, screenshots, etc.
+
+**Alerta**
+Recuerda que este repositorio es p\xFAblico, todos los issues y conversaciones 
+relacionadas son p\xFAblicas y visibles a todo el mundo.
+Si necesitas compartirnos informaci\xF3n de forma privada, NO lo hagas a trav\xE9s de este repo, 
+nos pondremos en contacto para poder dar soporte a trav\xE9s de un canal privado.`},{fname:"question.pt.md",id:"question",lang:"pt",headers:{name:"Fa\xE7a uma pergunta",about:"Voc\xEA tem alguma pergunta ou coment\xE1rio?",title:"[Question] Substitua este texto pelo t\xEDtulo",labels:"question",assignees:""},body:`Neste espa\xE7o voc\xEA pode adicionar o contexto necess\xE1rio: texto, screenshots, etc.
+
+**Alerta**
+Lembre-se que este reposit\xF3rio \xE9 p\xFAblico, todos os problemas e conversas
+relacionados s\xE3o p\xFAblicos e vis\xEDveis para todos.
+Se voc\xEA precisar compartilhar informa\xE7\xF5es conosco em particular, N\xC3O fa\xE7a isso por meio deste reposit\xF3rio,
+Entraremos em contato com voc\xEA para poder fornecer suporte por meio de um canal privado.`}],b={es:{techSupport:"Tech support",close:"Cerrar"},pt:{techSupport:"Tech support",close:"Fechar"}},g=r=>n=>b[r][n]||n,v=r=>`
 
 ***
 
@@ -39,11 +106,11 @@ ocurre el problema.`},{fname:"question.es.md",id:"question",lang:"es",headers:{n
 **Location**: \`${window.location}\`
 **User Agent**: \`${navigator.userAgent}\`
 **Navigator Language**: \`${navigator.language}\`
-**Selected Language**: \`${t}\`
-`,y=(t,r)=>({title:e,body:u,labels:s,assignees:c,template:a})=>{const l={...!!e&&{title:e},...!!s&&{labels:s},...!!c&&{assignees:c},...!!a&&{template:a},body:`${u}${h(r)}`},d=Object.keys(l).reduce((i,o)=>`${i?`${i}&`:""}${o}=${encodeURIComponent(`${l[o]}`)}`,"");return`https://github.com/${t}/issues/new?${d}`};class f extends HTMLElement{constructor(){super()}connectedCallback(){const r=this.getAttribute("repo")||"Laboratoria/tech-support",e=this.getAttribute("lang")||"es";if(!["es","pt"].includes(e))return alert(`[TechSupport] Unsupported lang: ${e}`);const u=m.filter(n=>n.lang===e),s=b(e),c=y(r,e),a=this.attachShadow({mode:"open"}),l=u.reduce((n,p)=>{const q=c({...p.headers,body:p.body});return`${n}
+**Selected Language**: \`${r}\`
+`,h=(r,n)=>({title:e,body:u,labels:t,assignees:c,template:a})=>{const i={...!!e&&{title:e},...!!t&&{labels:t},...!!c&&{assignees:c},...!!a&&{template:a},body:`${u}${v(n)}`},p=Object.keys(i).reduce((l,s)=>`${l?`${l}&`:""}${s}=${encodeURIComponent(`${i[s]}`)}`,"");return`https://github.com/${r}/issues/new?${p}`};class q extends HTMLElement{constructor(){super()}connectedCallback(){const n=this.getAttribute("repo")||"Laboratoria/tech-support",e=this.getAttribute("lang")||"es";if(!["es","pt"].includes(e))return alert(`[TechSupport] Unsupported lang: ${e}`);const u=m.filter(o=>o.lang===e),t=g(e),c=h(n,e),a=this.attachShadow({mode:"open"}),i=u.reduce((o,d)=>{const f=c({...d.headers,body:d.body});return`${o}
           <li>
-            <a href="${q}" target="_blank">
-              ${p.headers.name}
+            <a href="${f}" target="_blank">
+              ${d.headers.name}
             </a>
           </li>
         `},"");a.innerHTML=`
@@ -92,11 +159,11 @@ ocurre el problema.`},{fname:"question.es.md",id:"question",lang:"es",headers:{n
         border-radius: 0.4rem;
       }
       </style>
-      <a class="open-btn" href="#" title="${s("techSupport")}">\u{1F6DF}</a>
+      <a class="open-btn" href="#" title="${t("techSupport")}">\u{1F6DF}</a>
       <div class="menu">
         <ul>
-          ${l}
+          ${i}
         </ul>
-        <a class="close-btn" href="#">\u2716\uFE0F ${s("close")}</a>
+        <a class="close-btn" href="#">\u2716\uFE0F ${t("close")}</a>
       </div>
-    `;const d=a.querySelector(".open-btn"),i=a.querySelector(".close-btn"),o=a.querySelector(".menu");d.addEventListener("click",n=>{n.preventDefault(),o.style.display=o.style.display==="flex"?"none":"flex"}),i.addEventListener("click",n=>{n.preventDefault(),o.style.display="none"})}}customElements.define("x-tech-support",f)})();
+    `;const p=a.querySelector(".open-btn"),l=a.querySelector(".close-btn"),s=a.querySelector(".menu");p.addEventListener("click",o=>{o.preventDefault(),s.style.display=s.style.display==="flex"?"none":"flex"}),l.addEventListener("click",o=>{o.preventDefault(),s.style.display="none"})}}customElements.define("x-tech-support",q)})();
--- widget/dist/tech-support-widget.js
+++ widget/dist/tech-support-widget.js
@@ -1,4 +1,4 @@
-const g = [
+const b = [
   {
     fname: "bug_report.es.md",
     id: "bug_report",
@@ -30,7 +30,52 @@ el problema.
 
 **Contexto adicional**
 Agrega cualquier otra cosa que pueda ayudar a entender el contexto en el que
-ocurre el problema.`
+ocurre el problema.
+
+**Alerta**
+Recuerda que este repositorio es p\xFAblico, todos los issues y conversaciones 
+relacionadas son p\xFAblicas y visibles a todo el mundo.
+Si necesitas compartirnos informaci\xF3n de forma privada, NO lo hagas a trav\xE9s de este repo, 
+nos pondremos en contacto para poder dar soporte a trav\xE9s de un canal privado.`
+  },
+  {
+    fname: "bug_report.pt.md",
+    id: "bug_report",
+    lang: "pt",
+    headers: {
+      name: "Reportar um problema",
+      about: "Informe um problema que voc\xEA identificou para nos ajudar a melhorar",
+      title: "[Bug] Substitua este texto pelo t\xEDtulo",
+      labels: "bug",
+      assignees: "",
+      lang: "es"
+    },
+    body: `**Descreva o problema**
+Descri\xE7\xE3o clara e concisa de qual \xE9 o problema.
+
+**Para jogar**
+Etapas para reproduzir o problema:
+1. V\xE1 para '...'
+2. Clique em '....'
+3. Role para baixo at\xE9 '....'
+4. Veja o erro
+
+**Comportamento esperado**
+Descri\xE7\xE3o clara e concisa do que voc\xEA esperava que acontecesse.
+
+**Screenshots**
+Se poss\xEDvel, adicione capturas de tela para ajudar a entender
+o problema.
+
+**Contexto adicional**
+Adicione qualquer outra coisa que possa ajudar a entender o contexto em que
+o problema ocorre.
+
+**Alerta**
+Lembre-se que este reposit\xF3rio \xE9 p\xFAblico, todos os problemas e conversas
+relacionados s\xE3o p\xFAblicos e vis\xEDveis para todos.
+Se voc\xEA precisar compartilhar informa\xE7\xF5es conosco em particular, N\xC3O fa\xE7a isso por meio deste reposit\xF3rio,
+Entraremos em contato com voc\xEA para poder fornecer suporte por meio de um canal privado.`
   },
   {
     fname: "feature_request.es.md",
@@ -51,12 +96,49 @@ frustro cuando [...]
 Descripci\xF3n clara y concisa de qu\xE9 te gustar\xEDa que ocurra.
 
 **\xBFQu\xE9 alternativas has considerado?**
-Descripci\xF3n clara y concisa de otras solucioines alternativas que hayas
+Descripci\xF3n clara y concisa de otras soluciones alternativas que hayas
 considerado.
 
 **Contexto adicional**
 Agrega cualquier otra cosa que pueda ayudar a entender el contexto en el que
-ocurre el problema.`
+ocurre el problema.
+
+**Alerta**
+Recuerda que este repositorio es p\xFAblico, todos los issues y conversaciones 
+relacionadas son p\xFAblicas y visibles a todo el mundo.
+Si necesitas compartirnos informaci\xF3n de forma privada, NO lo hagas a trav\xE9s de este repo, 
+nos pondremos en contacto para poder dar soporte a trav\xE9s de un canal privado.`
+  },
+  {
+    fname: "feature_request.pt.md",
+    id: "feature_request",
+    lang: "pt",
+    headers: {
+      name: "Sugerir uma melhoria",
+      about: "Proponha uma ideia de melhoria que voc\xEA gostaria de ver",
+      title: "[Melhoria] Substitua este texto pelo t\xEDtulo",
+      labels: "enhancement",
+      assignees: ""
+    },
+    body: `**A melhoria que voc\xEA deseja propor est\xE1 relacionada a um problema?**
+Descri\xE7\xE3o clara e concisa de qual \xE9 o problema. Por exemplo, Eu fico frustrado quando [...]
+
+**Descreva a solu\xE7\xE3o desejada**
+Descri\xE7\xE3o clara e concisa do que voc\xEA gostaria de ver acontecer.
+
+**Quais alternativas voc\xEA considerou?**
+Descri\xE7\xE3o clara e concisa de outras solu\xE7\xF5es alternativas que voc\xEA tem
+considerado.
+
+**Contexto adicional**
+Adicione qualquer outra coisa que possa ajudar a entender o contexto em que
+o problema ocorre.
+
+**Alerta**
+Lembre-se que este reposit\xF3rio \xE9 p\xFAblico, todos os problemas e conversas
+relacionados s\xE3o p\xFAblicos e vis\xEDveis para todos.
+Se voc\xEA precisar compartilhar informa\xE7\xF5es conosco em particular, N\xC3O fa\xE7a isso por meio deste reposit\xF3rio,
+Entraremos em contato com voc\xEA para poder fornecer suporte por meio de um canal privado.`
   },
   {
     fname: "question.es.md",
@@ -69,9 +151,34 @@ ocurre el problema.`
       labels: "question",
       assignees: ""
     },
-    body: "En este espacio puedes agregar el contexto necesario: texto, screenshots, etc."
+    body: `En este espacio puedes agregar el contexto necesario: texto, screenshots, etc.
+
+**Alerta**
+Recuerda que este repositorio es p\xFAblico, todos los issues y conversaciones 
+relacionadas son p\xFAblicas y visibles a todo el mundo.
+Si necesitas compartirnos informaci\xF3n de forma privada, NO lo hagas a trav\xE9s de este repo, 
+nos pondremos en contacto para poder dar soporte a trav\xE9s de un canal privado.`
+  },
+  {
+    fname: "question.pt.md",
+    id: "question",
+    lang: "pt",
+    headers: {
+      name: "Fa\xE7a uma pergunta",
+      about: "Voc\xEA tem alguma pergunta ou coment\xE1rio?",
+      title: "[Question] Substitua este texto pelo t\xEDtulo",
+      labels: "question",
+      assignees: ""
+    },
+    body: `Neste espa\xE7o voc\xEA pode adicionar o contexto necess\xE1rio: texto, screenshots, etc.
+
+**Alerta**
+Lembre-se que este reposit\xF3rio \xE9 p\xFAblico, todos os problemas e conversas
+relacionados s\xE3o p\xFAblicos e vis\xEDveis para todos.
+Se voc\xEA precisar compartilhar informa\xE7\xF5es conosco em particular, N\xC3O fa\xE7a isso por meio deste reposit\xF3rio,
+Entraremos em contato com voc\xEA para poder fornecer suporte por meio de um canal privado.`
   }
-], b = {
+], g = {
   es: {
     techSupport: "Tech support",
     close: "Cerrar"
@@ -80,7 +187,7 @@ ocurre el problema.`
     techSupport: "Tech support",
     close: "Fechar"
   }
-}, h = (t) => (r) => b[t][r] || r, y = (t) => `
+}, v = (r) => (n) => g[r][n] || n, h = (r) => `
 
 ***
 
@@ -89,41 +196,41 @@ ocurre el problema.`
 **Location**: \`${window.location}\`
 **User Agent**: \`${navigator.userAgent}\`
 **Navigator Language**: \`${navigator.language}\`
-**Selected Language**: \`${t}\`
-`, f = (t, r) => ({
+**Selected Language**: \`${r}\`
+`, q = (r, n) => ({
   title: e,
   body: u,
-  labels: s,
+  labels: t,
   assignees: c,
   template: a
 }) => {
-  const l = {
+  const i = {
     ...!!e && { title: e },
-    ...!!s && { labels: s },
+    ...!!t && { labels: t },
     ...!!c && { assignees: c },
     ...!!a && { template: a },
-    body: `${u}${y(r)}`
-  }, d = Object.keys(l).reduce(
-    (i, o) => `${i ? `${i}&` : ""}${o}=${encodeURIComponent(`${l[o]}`)}`,
+    body: `${u}${h(n)}`
+  }, p = Object.keys(i).reduce(
+    (l, s) => `${l ? `${l}&` : ""}${s}=${encodeURIComponent(`${i[s]}`)}`,
     ""
   );
-  return `https://github.com/${t}/issues/new?${d}`;
+  return `https://github.com/${r}/issues/new?${p}`;
 };
-class q extends HTMLElement {
+class f extends HTMLElement {
   constructor() {
     super();
   }
   connectedCallback() {
-    const r = this.getAttribute("repo") || "Laboratoria/tech-support", e = this.getAttribute("lang") || "es";
+    const n = this.getAttribute("repo") || "Laboratoria/tech-support", e = this.getAttribute("lang") || "es";
     if (!["es", "pt"].includes(e))
       return alert(`[TechSupport] Unsupported lang: ${e}`);
-    const u = g.filter((n) => n.lang === e), s = h(e), c = f(r, e), a = this.attachShadow({ mode: "open" }), l = u.reduce(
-      (n, p) => {
-        const m = c({ ...p.headers, body: p.body });
-        return `${n}
+    const u = b.filter((o) => o.lang === e), t = v(e), c = q(n, e), a = this.attachShadow({ mode: "open" }), i = u.reduce(
+      (o, d) => {
+        const m = c({ ...d.headers, body: d.body });
+        return `${o}
           <li>
             <a href="${m}" target="_blank">
-              ${p.headers.name}
+              ${d.headers.name}
             </a>
           </li>
         `;
@@ -176,20 +283,20 @@ class q extends HTMLElement {
         border-radius: 0.4rem;
       }
       </style>
-      <a class="open-btn" href="#" title="${s("techSupport")}">\u{1F6DF}</a>
+      <a class="open-btn" href="#" title="${t("techSupport")}">\u{1F6DF}</a>
       <div class="menu">
         <ul>
-          ${l}
+          ${i}
         </ul>
-        <a class="close-btn" href="#">\u2716\uFE0F ${s("close")}</a>
+        <a class="close-btn" href="#">\u2716\uFE0F ${t("close")}</a>
       </div>
     `;
-    const d = a.querySelector(".open-btn"), i = a.querySelector(".close-btn"), o = a.querySelector(".menu");
-    d.addEventListener("click", (n) => {
-      n.preventDefault(), o.style.display = o.style.display === "flex" ? "none" : "flex";
-    }), i.addEventListener("click", (n) => {
-      n.preventDefault(), o.style.display = "none";
+    const p = a.querySelector(".open-btn"), l = a.querySelector(".close-btn"), s = a.querySelector(".menu");
+    p.addEventListener("click", (o) => {
+      o.preventDefault(), s.style.display = s.style.display === "flex" ? "none" : "flex";
+    }), l.addEventListener("click", (o) => {
+      o.preventDefault(), s.style.display = "none";
     });
   }
 }
-customElements.define("x-tech-support", q);
+customElements.define("x-tech-support", f);
